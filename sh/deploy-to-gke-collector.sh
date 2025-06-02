9fcc4ff6f57e65ce71602133331c075a39102d84
howhow1201 <131748154+howhow1201@users.noreply.github.com>
2025-06-02 15:46:56 +0800

    Create deploy-to-gke-collector.sh

---
+++ sh/deploy-to-gke-collector.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 透過 gcloud 取得 GKE 認證並部署 yaml 到 GKE
+# 請先設定下方三個變數
+
+PROJECT_ID="cloud-sre-poc-447001"
+REGION="asia-east1"
+CLUSTER_NAME="tracing-gke-cluster"
+
+# 取得 GKE 認證
+ gcloud container clusters get-credentials "$CLUSTER_NAME" --region "$REGION" --project "$PROJECT_ID"
+
+# 自動建立最新 cloudsql-sa-key secret（每次都覆蓋）
+#kubectl delete secret cloudsql-sa-key --ignore-not-found
+#kubectl create secret generic cloudsql-sa-key --from-file=credentials.json=cloudsql-sa-key.json
+
+
+# 部署 Deployment & Service yaml
+kubectl create namespace opentelemetry
+cd ..
+kubectl apply -f yaml/ClusterRole.yaml
+kubectl apply -f yaml/config.yaml
+kubectl apply -f yaml/collector-DaemonSet.yaml
+#kubectl rollout restart deployment app3
+#kubectl apply -f yaml/app3-service.yaml
+#kubectl apply -f yaml/app4-deployment.yaml
+#kubectl rollout restart deployment app4
+#kubectl apply -f yaml/app4-service.yaml
+
+echo "\n已完成 opentelemetry 部署！可用 kubectl get pods, kubectl get svc 檢查狀態。"
