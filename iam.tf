1eeb060919654cab03c4de9c1b8464e0773137a3
鄧仁豪 <dengrenhao@dengrenhaodeMacBook-Air.local>
2025-06-04 11:25:12 +0800

    upd

---
+++ iam.tf
@@ -0,0 +1,26 @@
+resource "google_service_account" "otel_sa" {
+  account_id   = "otel-collector" # 這會產生 otel-svc-account@PROJECT_ID.iam.gserviceaccount.com
+  display_name = "OpenTelemetry Service Account"
+}
+
+resource "google_project_iam_member" "otel_logging" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.otel_sa.email}"
+}
+
+resource "google_project_iam_member" "otel_monitoring" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.otel_sa.email}"
+}
+
+resource "google_project_iam_member" "otel_tracing" {
+  project = var.project_id
+  role    = "roles/cloudtrace.agent"
+  member  = "serviceAccount:${google_service_account.otel_sa.email}"
+}
+
+
+
+
