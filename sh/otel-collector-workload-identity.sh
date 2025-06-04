a9515d1bb9a451d2eece99217cb9786ee49e4a5b
howhow1201 <131748154+howhow1201@users.noreply.github.com>
2025-06-04 22:30:20 +0800

    Delete sh/otel-collector-workload-identity.sh

--- sh/otel-collector-workload-identity.sh
+++
@@ -1,9 +0,0 @@
-PROJECT_ID="cloud-sre-poc-447001"
-SERVICEACCOUNT="otel-collector"
-NAMESPACE="opentelemetry"
-
-kubectl create serviceaccount $SERVICEACCOUNT --namespace=$NAMESPACE
-
-gcloud iam service-accounts add-iam-policy-binding $SERVICEACCOUNT@$PROJECT_ID.iam.gserviceaccount.com --role roles/iam.workloadIdentityUser --member "serviceAccount:$PROJECT_ID.svc.id.goog[$NAMESPACE/$SERVICEACCOUNT]"
-
-kubectl annotate serviceaccount $SERVICEACCOUNT --namespace $NAMESPACE iam.gke.io/gcp-service-account=$SERVICEACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
