80f897e0971a259609a2bb9cad5d375a764ca27f
chien-ping <pingchien.biz@gmail.com>
2025-06-11 14:48:01 +0800

    feat: add k6 deployment with Workload Identity and Fluent Bit logging configuration

--- sh/deploy-to-gke-k6.sh
+++ sh/deploy-to-gke-k6.sh
@@ -9,6 +9,7 @@ REGION="asia-east1"                # GKE 集群所在區域
 CLUSTER_NAME="tracing-gke-cluster"         # GKE 集群名稱
 SERVICE_ACCOUNT="k6-gcp-sa"        # GCP 服務帳號名稱
 K6_YAML_PATH="./yaml/k6-deployment.yaml"  # k6 部署清單相對路徑
+K6_1_YAML_PATH="./yaml/k6-1-deployment.yaml"  # k6_1 部署清單相對路徑
 
 # 顏色設置
 GREEN='\033[0;32m'
@@ -46,6 +47,10 @@ gcloud iam service-accounts add-iam-policy-binding \
 # 應用 k6 部署
 echo -e "${YELLOW}部署 k6 到集群...${NC}"
 kubectl apply -f ${K6_YAML_PATH}
+
+# 應用 k6_1 部署
+echo -e "${YELLOW}部署 k6_1 到集群...${NC}"
+kubectl apply -f ${K6_1_YAML_PATH}
 
 echo -e "${GREEN}部署完成!${NC}"
 echo -e "${GREEN}提示: 您可以使用以下命令查看 k6 日誌:${NC}"
